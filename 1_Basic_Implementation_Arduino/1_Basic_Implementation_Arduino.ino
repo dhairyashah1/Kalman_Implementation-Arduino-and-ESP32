5122658563e7209b2cc9b706c3be2e081aa9e8ae
dhairyashah1 <dhairyashah110501@gmail.com>
2021-06-15 00:23:12 +0530

    Added output images

--- 1_Basic_Implementation_Arduino/1_Basic_Implementation_Arduino.ino
+++ 1_Basic_Implementation_Arduino/1_Basic_Implementation_Arduino.ino
@@ -1,12 +1,14 @@
-int batteryCapacity = 2500;     //capacity rating of battery in mAh
-float resistance = 10.0;        //measured resistance of the power resistor
-int cutoffVoltage = 1600;       //maximum battery voltage (in mV) that should not be exceeded
-float cutoffTemperatureC = 35;     //maximum battery temperature that should not be exceeded (in degrees C)
-//float cutoffTemperatureF = 95;     //maximum battery temperature that should not be exceeded (in degrees F)
-long cutoffTime = 46800000;      //maximum charge time of 13 hours that should not be exceeded
+int batteryCapacity = 2500;     //capacity rating of battery in mAh (eg)
+float resistance = 10.0;     //measured resistance of the power resistor (connected to battery)
+int cutoffVoltage = 4800;     //maximum battery voltage (in mV) that should not be exceeded = 4.8V (for 5V Li battery)
+float cutoffTemperatureC = 75;     //maximum battery temperature that should not be exceeded (in degrees C)
+//float cutoffTemperatureF = 167;     //maximum battery temperature that should not be exceeded (in degrees F) (F = 9*C/5 + 32)
+long cutoffTime = 60*1000;     //maximum charge time of 1 min that should not be exceeded (in seconds)
 
-int outputPin = 9;         //Output signal wire connected to digital pin 9
+int outputPin = 9;     // Output signal wire connected to digital pin 9 (Charging pin to battery)
 int outputValue = 150;     //value of PWM output signal 
+int outputFeedbackPin = 5; // feedback to read output
+float outputReadValue = 0;
 
 int analogPinOne = 0;     //first voltage probe connected to analog pin 1
 float valueProbeOne = 0;     //variable to store the value of analogPinOne
@@ -27,6 +29,8 @@ float batteryVoltage = 0;     //calculated voltage of battery
 float current = 0;     //calculated current through the load (in mA)
 float targetCurrent = batteryCapacity / 10;     //target output current (in mA) set at C/10 or 1/10 of the battery capacity per hour
 float currentError = 0;     //difference between target current and actual current (in mA)
+float SocPercentage = 0;
+
 
 void setup()
 {
@@ -34,16 +38,17 @@ void setup()
   pinMode(outputPin, OUTPUT);     // sets the pin as output
 }
 
+
+
 void loop()
-{
-    
+{   
   analogWrite(outputPin, outputValue);  //Write output value to output pin
 
   Serial.print("Output: ");     //display output values for monitoring with a computer
   Serial.println(outputValue); 
 
   valueProbeOne = analogRead(analogPinOne);    // read the input value at probe one
-  voltageProbeOne = (valueProbeOne*5000)/1023;     //calculate voltage at probe one in milliVolts
+  voltageProbeOne = (valueProbeOne*5000)/1023;     //calculate voltage at probe one in milliVolts => 10 bit ADC
   Serial.print("Voltage Probe One (mV): ");     //display voltage at probe one
   Serial.println(voltageProbeOne);  
   
@@ -52,11 +57,11 @@ void loop()
   Serial.print("Voltage Probe Two (mV): ");     //display voltage at probe two
   Serial.println(voltageProbeTwo);  
   
-  batteryVoltage = 5000 - voltageProbeTwo;     //calculate battery voltage
+  batteryVoltage = voltageProbeOne - voltageProbeTwo;     //calculate battery voltage 
   Serial.print("Battery Voltage (mV): ");     //display battery voltage
   Serial.println(batteryVoltage); 
 
-  current = (voltageProbeTwo - voltageProbeOne) / resistance;     //calculate charge current
+  current = (voltageProbeOne - voltageProbeTwo) / resistance;     //calculate charge current
   Serial.print("Target Current (mA): ");     //display target current 
   Serial.println(targetCurrent);  
   Serial.print("Battery Current (mA): ");     //display actual current
@@ -67,10 +72,10 @@ void loop()
   Serial.println(currentError);     
 
   valueProbeThree = analogRead(analogPinThree);    // read the input value at probe three  
-  tmp36Voltage = valueProbeThree * 5.0;     // converting that reading to voltage
-  tmp36Voltage /= 1024.0; 
- 
-  temperatureC = (tmp36Voltage - 0.5) * 100 ;     //converting from 10 mv per degree wit 500 mV offset to degrees ((voltage - 500mV) times 100)
+  tmp36Voltage = valueProbeThree * 5.0/1023;     // converting that reading to voltage
+
+  // currently not in use - formula depends on sensor used (temporary)
+  temperatureC = (tmp36Voltage - 0.05) * 100 ;     //converting from 10 mv per degree wit 50 mV offset to degrees ((voltage - 50mV) times 100)
   Serial.print("Temperature (degrees C) ");     //display the temperature in degrees C
   Serial.println(temperatureC); 
  
@@ -84,10 +89,10 @@ void loop()
   Serial.println();  
 
 
-
-  if(abs(currentError) > 10)     //if output error is large enough, adjust output
+// bounding range of current - can change (current varies -> temperature varies)
+  if(abs(currentError) > 50)     //if output error is large enough more than diff of 50mA, adjust output 
    {
-    outputValue = outputValue + currentError / 10;
+    outputValue = outputValue + currentError/2;
 
     if(outputValue < 1)    //output can never go below 0
      {
@@ -106,7 +111,7 @@ void loop()
   if(temperatureC > cutoffTemperatureC)     //stop charging if the battery temperature exceeds the safety threshold
    {
     outputValue = 0;
-    Serial.print("Max Temperature Exceeded");
+    Serial.println("Max Temperature Exceeded: ");
    }
    
   /*
@@ -119,14 +124,14 @@ void loop()
    if(batteryVoltage > cutoffVoltage)     //stop charging if the battery voltage exceeds the safety threshold
    {
     outputValue = 0;
-    Serial.print("Max Voltage Exceeded");
+    Serial.println("Max Voltage Exceeded: ");
    }  
  
-   if(millis() > cutoffTime)     //stop charging if the charge time threshold
+   if(millis() > cutoffTime)     //stop charging if the charge time exceeds threshold charging time
    {
     outputValue = 0;
-    Serial.print("Max Charge Time Exceeded");
+    Serial.println("Max Charge Time Exceeded");
    }  
 
-   delay(10000);     //delay 10 seconds before next iteration
+   delay(3000);     //delay 3 seconds before next iteration
 }
